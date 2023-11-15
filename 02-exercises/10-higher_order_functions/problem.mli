f85e6f2116e7829fcf28a10a0d311503e347f019
tbonza <tylers.pile@gmail.com>
2023-11-14 21:48:52 -0500

    got the higher order function to work

--- 02-exercises/10-higher_order_functions/problem.mli
+++ 02-exercises/10-higher_order_functions/problem.mli
@@ -3,3 +3,4 @@ open! Base
 val up_to : int -> (int -> int -> int) -> int -> int
 val simpler_sum : int list -> int
 val simpler_product : int list -> int
+val up_to : int list -> (int -> int list -> int list) -> int -> int list
